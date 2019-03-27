00768d0b2f27312a007c5f9e34a9bcd5c4becd84
Marie Berry (Leugermann) <marie@creativeos.io>
2019-03-27 10:01:24 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,5 +1,5 @@
 class Project
-  attr_reader :backer
+  attr_reader :project, :backers
   
   def initialize(project)
     @project = project
@@ -9,5 +9,17 @@ class Project
   def add_backer(instance)
     @backers << self
   
-  
+  class Project
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
+    backer.back_project(self) unless backer.backed_projects.include?(self)
+  end
+end
   
